c7d0d085ddb4d60ddc41b10361be739a83e36a0c
Szymon Stoklosa <https://github.com/stoklosaszymon>
2024-12-19 03:38:21 +0100

    add weather features

--- src/app/components/widgets/widget-weather/widget-weather.component.ts
+++ src/app/components/widgets/widget-weather/widget-weather.component.ts
@@ -1,15 +1,15 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, ElementRef, computed, effect, signal, viewChild } from '@angular/core';
 import { ApiService } from '../../../api.service';
 import { Observable, Subject, switchMap } from 'rxjs';
 import { NgClass, NgStyle } from '@angular/common';
+import Chart from 'chart.js/auto';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+import weatherJson from './assets/description.json'
 
-const URL = 'https://api.open-meteo.com/v1/forecast?current=temperature_2m,is_day,snowfall,weather_code,cloud_cover,wind_speed_10m,wind_direction_10m&hourly=temperature_2m';
-const WEATHER_CODES: WeatherCodes = {
-  0: 'Clear Sky',
-  1: 'MainlyClear',
-  2: 'PartlyCloudy',
-  3: 'Overcast'
-}
+const URL = 'https://api.open-meteo.com/v1/forecast?current=temperature_2m,relative_humidity_2m,precipitation,wind_speed_10m,is_day,snowfall,weather_code,cloud_cover,wind_speed_10m,wind_direction_10m&hourly=temperature_2m&daily=weather_code,temperature_2m_max,temperature_2m_min';
+
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
 @Component({
   selector: 'app-widget-weather',
   standalone: true,
@@ -19,9 +19,11 @@ const WEATHER_CODES: WeatherCodes = {
 })
 export class WidgetWeatherComponent {
 
+  chart: any = [];
   date = signal(new Date());
   temperature = signal(0);
-  isDay = false;
+  details = signal({ precipitation: 0, humidity: 0, windSpeed: 0 })
+  isDay = signal(true);
   showClouds = true;
   computedTime = computed(() => {
     const hours = this.date().getHours();
@@ -29,13 +31,27 @@ export class WidgetWeatherComponent {
     return `${hours}:${minutes}`;
   });
   dayOfWeek = computed(() => {
-    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    return daysOfWeek[this.date().getDay()]; 
+    return daysOfWeek[this.date().getDay()];
   })
+  dailyTempretures: { min: number, max: number, day: string, img: string }[] = [];
+
+  dayEffect = effect(() => {
+    if (this.isDay()) {
+      Chart.defaults.color = "#fff";
+    }
+  })
+
+  chartData: { temperatures: number[], time: string[] } = {
+    temperatures: [],
+    time: []
+  }
 
   data = new Subject<any>();
 
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService) {
+    Chart.register(ChartDataLabels);
+    Chart.defaults.color = "#000";
+  }
 
   ngOnInit() {
     this.fetchWeatherData();
@@ -65,18 +81,118 @@ export class WidgetWeatherComponent {
         console.log('resp', response);
         this.date.set(new Date(response.current.time));
         this.temperature.set(response.current.temperature_2m);
-        this.isDay = !!response.current.is_day
+        this.isDay.set(!!response.current.is_day);
+        this.details.set({
+          precipitation: response.current.precipitation,
+          humidity: response.current.relative_humidity_2m,
+          windSpeed: response.current.wind_speed_10m
+        })
+
+        this.chartData.temperatures = response.hourly.temperature_2m.filter((_, index) => index % 3 == 0).splice(0, 24);
+        this.chartData.time = response.hourly.time.splice(0, 24).filter((_, index) => index % 3 == 0).map(date => {
+          const time = new Date(date);
+          return `${('0' + time.getHours()).slice(-2)}:${('0' + time.getMinutes()).slice(-2)}`;
+        });
+        this.buildChart();
+
+        response.daily.temperature_2m_max
+        response.daily.temperature_2m_min
+        this.dailyTempretures = response.daily.time.map((t, i) => {
+          const weatherCode = response.daily.weather_code[i];
+          const weatherObj = JSON.parse(JSON.stringify(weatherJson));
+          return {
+            day: daysOfWeek[new Date(t).getDay()],
+            min: Math.round(response.daily.temperature_2m_min[i]),
+            max: Math.round(response.daily.temperature_2m_max[i]),
+            img: weatherObj[weatherCode].day.image
+          }
+        });
+
       }
     })
+  }
+
+  buildChart() {
+    this.chart = new Chart('canvas', {
+      type: 'line',
+      data: {
+        labels: this.chartData.time,
+        datasets: [
+          {
+            label: 'temperature',
+            data: this.chartData.temperatures,
+            fill: true,
+          }
+        ]
+      },
+      options: {
+        maintainAspectRatio: false,
+        responsive: true,
+        layout: {
+          padding: {
+            top: 20
+          }
+        },
+        scales: {
+          x: {
+            border: {
+              display: false,
+            },
+            grid: {
+              drawOnChartArea: false,
+              drawTicks: false,
+            }
+          },
+          y: {
+            min: 0,
+            display: false,
+            grid: {
+              drawOnChartArea: false
+            },
+          }
+        },
+        plugins: {
+          tooltip: {
+            enabled: false
+          },
+          legend: {
+            display: false,
+          },
+          datalabels: {
+            anchor: 'end',
+            align: 'top',
+            font: {
+              weight: 'bold'
+            }
+          }
+        },
+      },
+    }
+    );
   }
 }
 
 interface WeatherResponse {
   current: {
-    temperature_2m: number,
-    weather_code: number,
+    temperature_2m: number
+    weather_code: number
     time: Date
-    is_day: boolean
+    is_day: number
+    precipitation: number
+    relative_humidity_2m: number
+    wind_speed_10m: number
+  }
+
+  hourly: {
+    temperature_2m: number[]
+    time: string[]
+  }
+
+  daily: {
+    temperature_2m_max: number[]
+    temperature_2m_min: number[]
+    weather_code: number[]
+    time: string[]
   }
 }
 
