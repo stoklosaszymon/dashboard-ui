a76756025a35ae0967eae06766b4673e9a5f68fb
Szymon Stoklosa <https://github.com/stoklosaszymon>
2025-02-04 02:26:20 +0100

    disable edit mode on widget-menu exit

--- src/app/widgets-list/widgets-list.component.ts
+++ src/app/widgets-list/widgets-list.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { DragDropModule,CdkDragPreview } from '@angular/cdk/drag-drop';
 import { WidgetWeatherComponent } from '../components/widgets/widget-weather/widget-weather.component';
 import { NgComponentOutlet } from '@angular/common';
 import { StockWidget } from '../components/widgets/stock-widget/stock-widget.component';
+import { MatIconModule } from '@angular/material/icon';
+import { DashboardService } from '../dashboard.service';
 
 @Component({
   selector: 'app-widgets-list',
   standalone: true,
-  imports: [DragDropModule, NgComponentOutlet],
+  imports: [DragDropModule, NgComponentOutlet, MatIconModule],
   templateUrl: './widgets-list.component.html',
   styleUrl: './widgets-list.component.scss'
 })
 export class WidgetsListComponent {
+
+  dashboardService = inject(DashboardService);
 
   widgets = [
     { name: 'Weather', component: WidgetWeatherComponent },
@@ -24,5 +28,9 @@ export class WidgetsListComponent {
 
   onDragEnd(event: any): void {
     document.body.classList.remove('dragging')
+  }
+
+  close() {
+    this.dashboardService.toggleEditMode();
   }
 }
