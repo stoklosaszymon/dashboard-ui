7acffbd07b4130cb15bdb7b4e97547c8ae347d51
stoklosaszymon <stoklosa.szymon@gmail.com>
2025-02-24 22:46:01 +0100

    small fixes

--- src/app/components/widgets/coin/coin.component.ts
+++ src/app/components/widgets/coin/coin.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, ElementRef, effect, signal, viewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, computed, effect, signal, viewChild } from '@angular/core';
 import { WidgetBaseComponent } from '../../widget-base/widget-base.component';
 
 @Component({
@@ -12,9 +12,11 @@ export class CoinComponent extends WidgetBaseComponent {
 
   random = signal(Math.floor((Math.random() * 2) + 1))
   coin = viewChild<ElementRef<HTMLDivElement>>('coin');
+  v = signal<string[]>([])
 
   randomEff = effect(() => {
     if (this.random()) {
+      this.v.update( (v) =>  [...v, this.random() % 2 == 0 ? 'H' : 'T'])
       this.coin()?.nativeElement.getAnimations().forEach((anim) => {
         anim.cancel();
         anim.play();
